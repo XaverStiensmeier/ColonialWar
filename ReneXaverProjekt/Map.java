f32dd6e6ac0d10032b650a2f02c82966e7cd3b17
XaverStiensmeier <36056823+XaverStiensmeier@users.noreply.github.com>
2018-02-02 17:52:41 +0100

    Map ist besser 02.02.2018
    Map Zählschleife

--- ReneXaverProjekt/Map.java
+++ ReneXaverProjekt/Map.java
@@ -1,15 +1,13 @@
-
 public class Map {
-
-	public Map() {
-
-		Pair[][] map = new Pair[10][10];
-
-		for (Pair[] b : map) {
-			for (Pair c : b)
-
-				System.out.println(c);
-		}
-	}
-
-}
+  public static void main(String [] args) {
+    Pair[][] lMap = new Pair[5][5];           
+        for (int i = 0; i < lMap.length; i++) {
+          for (int i2 = 0; i2 < lMap.length; i2++)
+          {  
+            lMap[i][i2] = new Pair(0,null);
+            System.out.println(lMap[i][i2]);
+          }
+        }
+      }
+      
+  }
